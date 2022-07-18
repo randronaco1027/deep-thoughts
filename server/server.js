d759b2d546556b280cf355dfed61dec97e0e8b73
randronaco <randronaco1027@gmail.com>
2022-07-18 00:38:31 -0400

    21.3

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -16,6 +17,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
