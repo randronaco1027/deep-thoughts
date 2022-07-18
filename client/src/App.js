dbecef873757b79dea6fa795590ac2e9b5a7ea66
randronaco <randronaco1027@gmail.com>
2022-07-18 16:24:48 -0400

    21.5

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
@@ -15,11 +16,22 @@ import Home from './pages/Home';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+
 
 function App() {
   return (
